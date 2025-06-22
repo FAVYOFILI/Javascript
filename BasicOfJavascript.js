fcb99b20fd3803ba0be677d6d33a237fcdec9890
Favour Ofili <favourofili0326@gmail.com>
2025-06-22 22:55:23 +0100

    Add files via upload
    BASICS OF JAVASCRIPT

---
+++ BasicOfJavascript.js
@@ -0,0 +1,21 @@
+let boy ="Ade";
+console.log(boy)
+
+let num=10;
+console.log(num)
+
+let gender ="male" 
+console.log(gender)
+let num2 =5;
+let num3 =7
+
+//ARITHMETIC OPERATORS;
+console.log(num2 + num3)
+console.log(5 - 2)
+console.log(2 *3)
+console.log(6 / 2)
+console.log(10 % 3)
+console.log(2 ** 3)
+
+//ASSIGMENT OPERATOR
+//let names =
